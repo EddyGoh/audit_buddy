ed7f9e83407078468ba851d6b2b63b1af3fd59a5
Eddy Goh <eddygoh81work@gmail.com>
2024-10-15 10:38:45 +0800

    updates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from openai import OpenAI
 from crewai_tools import PDFSearchTool, SerperDevTool, ScrapeWebsiteTool
-from crewai import Agent, Task, Crew
+from crewai import Agent, Task, Crew, LLM
 from PIL import Image
 
 import pysqlite3
@@ -15,8 +15,23 @@ OPENAI_MODEL_NAME = os.environ.get('OPENAI_MODEL_NAME')
 
 logo_image = Image.open("./image/logo.png")
 
-client = OpenAI(api_key=API_KEY)
-tool_pdf = PDFSearchTool()
+llm = LLM(
+    model="openai/"+OPENAI_MODEL_NAME,
+    api_key=API_KEY,
+    base_url="https://litellm.govtext.gov.sg/",
+    default_headers={"user-agent":"Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"}
+)
+
+tool_pdf = PDFSearchTool(
+    config=dict(
+        embedder=dict(
+            provider="openai",
+            config=dict(
+                model="text-embedding-3-large-prd-gcc2-lb"
+            ),
+        ),
+    )
+)
 tool_search = SerperDevTool(n_results=5, country="sg")
 tool_webscrape = ScrapeWebsiteTool()
 
@@ -32,6 +47,11 @@ st.image(logo_image, width=200)
 topic = st.text_input("Enter Audit Topic [For example: Baby Bonus Scheme]:")
 uploaded_files = st.file_uploader("Upload PDF files that may potentially contain past audit findings", accept_multiple_files=True, type="pdf")
 
+domain = st.multiselect(
+    "Focused websites to search through",
+    ["Hansard SG","Gov.sg","Reddit","Hardwarezone Forums"],
+    ["Hansard SG", "Gov.sg"],
+)
 
 if st.button("Let's Go, Buddy!!!") and topic and uploaded_files:
     os.makedirs("tempdir", exist_ok=True)
@@ -39,67 +59,81 @@ if st.button("Let's Go, Buddy!!!") and topic and uploaded_files:
 
     auditor = Agent(
         role="auditor",
-        goal=f"Generate Audit Planning Memo on {topic} using inputs from researcher and past audit findings from the audit assistant.",
+        goal=f"Generate the Audit Planning Memo on {topic} based on inputs from researcher and past audit findings and financial information from the audit assistant.",
         backstory=f"You are an auditor. You need to consider the inputs from the researcher and audit assistant to determine what to write for the audit planning memo and risk assessment",
         allow_delegation=True,
-        max_iter=5,
+        max_iter=15,
+        verbose=True,
+        llm = llm
     )
 
     researcher = Agent(
         role="researcher",
-        goal=f"From the internet, research and analyse and sieve out all information related to {topic} only.",
-        backstory=f"As a researcher, navigating and extracting critical information is crucial. You are assisting the auditor to research on information relevant to {topic}.",
+        goal=f"""From the internet, research and analyse and sieve out information related to {topic} only in the Singapore context. 
+        Think step by step and extract : regulatory information, potential audit findings, fraud cases and reasons why.""",
+        backstory=f"As a researcher, navigating and extracting critical information is crucial. You are assisting the auditor to research on top 10 sources most relevant to {topic} in the Singapore context.",
         allow_delegation=False,
-        max_iter=15
+        verbose=True,
+        max_iter=15,
+        llm = llm
     )
 
     audit_assistant = Agent(
         role="audit assistant",
-        goal=f"From PDF extract the audit findings pertaining to {topic}.",
-        backstory=f"You are an audit assistant. Your job is to extract the past audit findings relating to {topic}.",
+        goal=f"From PDF, extract the audit findings or financial information pertaining to {topic} only.",
+        backstory=f"You are an audit assistant. Your job is to extract the past audit findings or financial statements relating to the {topic}. Output could either be in text for past audit findings or JSON for financial statements.",
         allow_delegation=False,
-        max_iter=15
+        verbose=True,
+        max_iter=15,
+        llm = llm
     )
 
     task_search = Task(
-        description=f"Returns a list of 5 websites that contain the most relevant information, in the context of Singapore, that are related to regulations on {topic} only. Ignore urls that ends with .pdf.",
-        expected_output=f"a list of 5 websites",
+        description=f"Search for the top websites that contain the most relevant information, in the context of Singapore, that are related to regulations on {topic} only.",
+        expected_output=f"Returns a list of top websites that contain most relevant regulations related information, in the context of Singapore, on {topic} only. The output must be in JSON format with the description as the key and url as value.",
+        agent=researcher,
+        tools=[tool_search],
+    )
+
+    task_focused_search = Task(
+        description=f"Search for websites from {domain} that contain the most relevant information, in the context of Singapore, that are related to regulations on {topic} only.",
+        expected_output=f"Returns a list of top websites from {domain} that contain most relevant information, in the context of Singapore, on {topic} only. The output must be in JSON format with the description as the key and url as value.",
         agent=researcher,
         tools=[tool_search],
     )
 
     task_websitesearch = Task(
-        description=f"""1. Scrape the website for all regulations related to {topic} only
-                      2. Summarize the information scraped""",
-        expected_output=f"a summary of all regulations related to {topic}.",
+        description=f"Scrape websites for all regulations and information related to {topic} only. For urls ending with .PDF, use readpdf tool",
+        expected_output=f"A markdown document with a summary of information related to {topic}. Include the sources (description and URL/filename) used.",
         agent=researcher,
-        tools=[tool_webscrape],
-        context=[task_search]
+        tools=[tool_webscrape,tool_pdf],
+        context=[task_search,task_focused_search]
     )
 
-    task_extract_findings = Task(
-        description=f"For each file path in {file_paths}, read the pdf and extract all audit findings pertaining to {topic}. Provide citation",
-        expected_output=f"A list of audit findings pertaining to {topic} with citation",
+    task_readpdf = Task(
+        description=f"For each uploaded file path in {file_paths}, read the pdf and extract all audit findings pertaining to {topic}. Provide citation",
+        expected_output=f"A list of audit findings pertaining to {topic}",
         tools=[tool_pdf],
         agent=audit_assistant
     )
 
     task_write = Task(
-        description=f"""1. Use the content to craft an audit planning memo and risk assessment based on {topic}.
-                        2. Use the content to craft an audit risk assessment based on {topic}.
-                        3.Write in an engaging manner.
-                        4. Use common style used in audit planning memos.""",
-        expected_output="""1. Section 1. Professional written audit planning memo
-                           2. Section 2. Professional written audit risk assessment 
-                           3. Each section should have minimum of 2 and maximum of 5 paragraphs.""",
+        description=f"""
+        1. Use the content from web searches and files to write an audit planning memo and risk assessment based on {topic} for the team to work on.
+        2. Provide the background of the audit topic using the context provided
+        3. Include the sources used - websites URLs and file names of files used.
+        4. Proofread for grammatical errors and alignment with the common style used in audit planning memos.""",
+        expected_output="""
+        A well-written audit planning memo and risk assessment in markdown format.""",
         agent=auditor,
-        context=[task_websitesearch, task_extract_findings],
+        context=[task_websitesearch, task_readpdf],
         output_file="APM.md"
     )
 
     crew = Crew(
         agents=[auditor, researcher, audit_assistant],
-        tasks=[task_search, task_websitesearch, task_extract_findings, task_write],
+        tasks=[task_search, task_focused_search, task_websitesearch, task_readpdf, task_write],
+        verbose=True
     )
 
     with st.spinner("Generating Audit Planning Memo...[It will take a while.]"):
@@ -107,21 +141,18 @@ if st.button("Let's Go, Buddy!!!") and topic and uploaded_files:
 
     st.success("Audit Planning Memo generated successfully!")
 
-    st.subheader("Results:")
+    st.subheader("Raw Output")
     st.text(result.raw)
-
-    st.subheader("List of Websites:")
-    st.text(result.tasks_output[0])
-
-    st.subheader("Information extracted from websites:")
-    st.text(result.tasks_output[1])
-
-    st.subheader("Information extracted from PDFs:")
-    st.text(result.tasks_output[2])
 
     st.subheader("Token Usage")
     st.text(result.token_usage)
 
+    for i in range(len(result.tasks_output[:-1])):
+        st.subheader(f"Task {i+1} Output")
+        st.text(result.tasks_output[i])
+
+    st.subheader(f"Final Output")
+    st.text(result.tasks_output[-1])
     if os.path.exists("APM.md"):
         with open("APM.md", "r") as f:
             apm_content = f.read()
