78df98886abd4f5648c3a67987c4e2b2610b2c5c
YCH379 <yapchao@hotmail.com>
2024-10-18 17:10:16 +0800

    added flowchart

--- pages/about_us.py
+++ pages/about_us.py
@@ -14,9 +14,51 @@ st.set_page_config(
 
 st.title("About this App")
 
-st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+# Write the objectives using HTML text using st.markdown
+st.subheader("Objectives")
 
-with st.expander("How to use this App"):
-    st.write("1. Enter your prompt in the text area.")
-    st.write("2. Click the 'Submit' button.")
-    st.write("3. The app will generate a text completion based on your prompt.")
+obj_str = """
+<p style="text-align: justify;">
+    Audit planning and scoping are critical as they set the stage for an effective and efficient audit process. It is essential to carry out a preliminary study and risk assessment during this phase to identify potential areas of concern and tailor the audit strategy accordingly. This ensures that the audit focuses on the most significant areas, enhancing its effectiveness. However, this phase involves gathering a voluminous amount of information from various sources and integrating the information to properly identify all risk areas. This app utilizes LLM, specifically using a multi-agent framework from CrewAI, to assist AGO auditors in the audit planning and scoping phase of an audit project.
+</p>
+"""
+
+st.markdown(obj_str, unsafe_allow_html=True)
+
+
+# Write the project scope using HTML text using st.markdown
+st.subheader("Project Scope")
+
+scope_str = """
+<p style="text-align: justify;">
+    Although the app is tailored to Selective audits conducted by AGO, it could reasonably be used for other types of audit projects.
+</p>
+"""
+
+st.markdown(scope_str, unsafe_allow_html=True)
+
+
+# Write the data sources using HTML text using st.markdown
+st.subheader("Data Sources")
+
+sources_str = """
+<p style="text-align: justify;">
+    The data used would be from the user and information obtained in the internet through web scrapping.
+</p>
+"""
+
+st.markdown(sources_str, unsafe_allow_html=True)
+
+
+
+# Write the features using HTML text using st.markdown
+st.subheader("Features")
+
+features_str = """
+<p style="text-align: justify;">
+    The app requires the user to provide the audit topic, past audit findings in PDF format, and optional websites for information searches. The app searches the internet and the provided websites for other information related to the audit topic. With all the gathered information, it outputs the risk assessment and proposed audit steps.
+</p>
+"""
+
+st.markdown(features_str, unsafe_allow_html=True)
+
--- pages/methodology.py
+++ pages/methodology.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 from helper_functions.utility import check_password  
     
 # Check if the password is correct.  
@@ -7,16 +8,24 @@ if not check_password():
 
 # region <--------- Streamlit App Configuration --------->
 st.set_page_config(
-    layout="centered",
+    layout="wide",
     page_title="My Streamlit App"
 )
 # endregion <--------- Streamlit App Configuration --------->
 
 st.title("About this App")
 
-st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to generate text completions.")
+st.write("This is a Streamlit App that assist AGO auditors in the audit planning and scoping phase of an Selective audit project. Enter the audit topic and upload the past audit observations in pdf format. The app would analyse the past audit observations and search the internet (you can indicate the websites to search too!) for other relevant information and provides a draft of the risk assessment and possible audit steps.")
 
-with st.expander("How to use this App"):
-    st.write("1. Enter your prompt in the text area.")
-    st.write("2. Click the 'Submit' button.")
-    st.write("3. The app will generate a text completion based on your prompt.")
+with st.expander("How to use this App", expanded = True):
+    st.write("1. Enter the audit topic.")
+    st.write("2. Upload past audit observations in pdf format.")
+    st.write("3. Indicate any websites of interest that you want the app to search information for (Some examples are provided).")
+    st.write("4. Click the 'Let's go, Buddy!!!' icon.")
+    st.write("5. Be patient and wait for the download button to appear to download the result.")
+
+st.write("The app uses Large Language Model, specifically the crewAI multi-agent framework, to produce the output. For more information on the crewAI multi-agent framework, refer to https://docs.crewai.com/introduction. The below flowchart illustrate the process flow of the app.")
+
+flowchart_image = Image.open("./image/flowchart.jpeg")
+
+st.image(flowchart_image, width = 1200)
